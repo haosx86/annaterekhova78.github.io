c14658620b4e7e3b5fc96123ea8339b6ffc9a2eb
Vladimir Terekhov <haosx86@gmail.com>
2021-09-04 23:45:47 +0300

    update routing; add WebP gulp pipeline; add mobile menu; implement mobile version of the first section of home

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
+const cwebp = require('gulp-cwebp');
+const cleanCSS = require('gulp-clean-css');
 
 const exec = require('child_process').exec;
 
@@ -22,12 +24,13 @@ function initBrowserSync() {
   })
 
   watch('./src/**/*.less', styles);
-  watch('./static/**/*', staticCopy);
+  watch('./static/font/**/*', fontCopy);
+  watch('./static/img/**/*', convertImages);
   watch('./src/**/*.+(html|njk)', njk);
 }
 
 function njk() {
-  return src("./src/pages/*.njk")
+  return src("./src/pages/**/*.njk")
     .pipe(nunjucks.compile({}, {
       env: new nunjucks_lib.Environment(new nunjucks_lib.FileSystemLoader('./src/particials'))
     }))
@@ -41,27 +44,37 @@ function styles() {
     .pipe(less())
     .pipe(concat('styles.css'))
     .pipe(autoprefixer({ overrideBrowserslist: ['last 4 versions'], grid: true }))
+    .pipe(cleanCSS())
     .pipe(dest('./dist'))
     .pipe(browserSync.stream())
 }
 
-function staticCopy() {
-  return src('./static/**/*')
-    .pipe(dest("./dist"))
+function fontCopy() {
+  return src('./static/font/**/*')
+    .pipe(dest("./dist/font"))
     .pipe(browserSync.stream())
+}
+
+function convertImages() {
+  return src('./static/img/**/*')
+  .pipe(cwebp())
+  .pipe(dest('./dist/img'))
+  .pipe(browserSync.stream())
 }
 
 exports.initBrowserSync = initBrowserSync
 exports.njk = njk
 exports.styles = styles
-exports.staticCopy = staticCopy
-exports.build = series(clean, parallel(njk, styles, staticCopy))
+exports.fontCopy = fontCopy
+exports.convertImages = convertImages
+exports.build = series(clean, parallel(njk, styles, fontCopy, convertImages))
 exports.default = series(
   clean,
   parallel(
     njk,
     styles,
-    staticCopy
+    fontCopy,
+    convertImages
   ),
   initBrowserSync
 )
